fc3c59544b2a8cedd18946ef3331c4992fdeeb1e
Talal Talhouk <talal@sbcglobal.net>
2016-07-06 15:12:48 -0700

    complete step 2

--- server/server.js
+++ server/server.js
@@ -18,10 +18,54 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
+
 var lions = [];
 var id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
+app.get('/lions', function(req, res){
+  res.json(lions)
+});
+app.post('/lions', function(req, res) {
+  var lion = req.body;
+  id += 1
+  lion.id = id + '';
+  lions.push(lion);
+  res.send(lion);
+});
+
+// get the parameters from the route
+app.get('/lions/:id', function(req, res) {
+  var lion = _.find(lions, {id: req.params.id});
+
+  res.json(lion || {});
+});
+
+app.put('/lions/:id', function(req, res) {
+  var update = req.body;
+  if (update.id){
+    delete(update.id)
+  }
+  var lion = _.findIndex(lions, {id: req.params.id});
+  if (!lions[lion]){
+    res.send();
+  } else {
+    var updatedLion = _.assign(lions[lion], update);
+    res.json(updatedLion);
+  }
+});
+
+app.delete('/lions/:id',function(req, res) {
+  var lion = _.findIndex(lions, {id: req.params.id});
+  if (!lions[lion]){
+    res.send();
+  } else {
+    var deletedLion =  lions[lion];
+    lions.splice(lion, 1);
+    res.json(deletedLion)
+  }
+});
+
 
 app.listen(3000);
 console.log('on port 3000');
